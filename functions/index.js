2f143ca2f77a3c94bccb3b7dfe42ebb5f2dff391
kou72 <genpokopokopoko@gmail.com>
2020-06-30 01:59:08 +0900

    mod functions (post)

--- functions/index.js
+++ functions/index.js
@@ -4,5 +4,10 @@ const functions = require("firebase-functions");
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+  if (request.method !== "POST") {
+    response.send("This is not post request");
+  }
+
+  console.log(request);
+  response.send("OK");
 });
